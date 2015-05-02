62cefcd2a9b90130f0c48cc595aaa55bf11f0036
samvantran <samvantran@gmail.com>
2015-05-02 13:49:15 -0700

    Complete tutorial
    - Add private methods for security
    - Add facebook oauth
    - Set up pub/sub; removed autopublish
    - Enable private posts via toggle-private and isOwner

--- simple-todos.js
+++ simple-todos.js
@@ -2,10 +2,26 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
+
+  Meteor.subscribe('tasks');
+
   Template.body.helpers({
+
     tasks: function() {
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
+      if (Session.get("hideCompleted")) {
+        // if hide compeleted is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function() {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function() {
+      return Tasks.find({checked: {$ne: true}}).count();
+    },
   });
 
   Template.body.events({
@@ -14,25 +30,91 @@ if (Meteor.isClient) {
       console.log(event);
       var text = event.target.text.value;
 
-      Tasks.insert({
-        text: text,
-        createdAt: new Date()
-      });
+      Meteor.call("addTask", text);
 
-      // Clear form
-      event.target.text.value = "";
+      event.target.text.value = "";   // Clear form
 
-      // Prevent default form submit
-      return false;
+      return false;     // Prevent default form submit
+    },
+
+    "change .hide-completed input": function(event) {
+      Session.set("hideCompleted", event.target.checked);
     }
   });
 
   Template.task.events({
     "click .toggle-checked": function() {
-      Tasks.update(this._id, {$set: {checked: !this.checked}});
+      Meteor.call("setChecked", this._id, !this.checked);
     },
     "click .delete": function() {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
+    },
+    "click .toggle-private": function() {
+      Meteor.call("setPrivate", this._id, !this.private);
     }
   });
+
+  Template.task.helpers({
+    isOwner: function() {
+      return this.owner === Meteor.userId();
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
+
+Meteor.methods({
+  addTask: function(text) {
+    // Make sure the user is logged in before inserting a task
+    if (!Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),            // current time
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username  // username of user
+    });
+  },
+  deleteTask: function(taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.remove(taskId);
+  },
+  setChecked: function(taskId, setChecked) {
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+    Tasks.update(taskId, { $set: { checked: setChecked} })
+  },
+  setPrivate: function(taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate}});
+  }
+})
+
+if (Meteor.isServer) {
+  Meteor.publish('tasks', function() {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true}},
+        { owner: this.userId }
+      ]
+    });
+  })
+}
+
+
